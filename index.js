4b3fb02cf3e956268a2643c4a05683b2e271924c
saadchadu <saadchadu@gmail.com>
2023-01-08 18:28:44 +0530

    Sentence case button ready

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 5,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 4,
+  spaceBetween: 5,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+let inputBox = document.querySelector("textarea");
+
+function sentenceBtnClick() {
+  let inputValue = inputBox.value;
+
+  function capitalizeFirstLetter() {
+    return inputValue[0].toUpperCase() + inputValue.slice(1);
+  }
+  
+  let sentence = inputValue;
+  inputBox.value = capitalizeFirstLetter(); 
+  
+}
+
+
