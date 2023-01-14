52fa2701f7a7917ee08211bc0e71c90280bc0c8c
saadchadu <saadchadu@gmail.com>
2023-01-14 21:51:35 +0530

    slider issue fixed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 5,
+  slidesPerView: 5,
+  spaceBetween: 1,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
+
+
 
 
 
