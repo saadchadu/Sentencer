f4302ebc34f108f31d21f1f96bbbe80c669f8851
saadchadu <saadchadu@gmail.com>
2023-01-17 14:43:19 +0530

    lorem ipsum test

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ let swiper = new Swiper(".mySwiper", {
   },
 });
 
-
-
-
-
-
 let inputBox = document.querySelector("textarea");
-
-
 
 //Sentence Case
 
@@ -26,13 +19,12 @@ function sentenceBtnClick() {
     var lowercase = sentence.toLowerCase();
 
     var capitalized = lowercase[0].toUpperCase() + lowercase.slice(1);
-  
+
     return capitalized;
   }
   var sentence = inputValue;
-var modifiedSentence = makeSentenceCase(sentence);
-inputBox.value = modifiedSentence; 
-
+  var modifiedSentence = makeSentenceCase(sentence);
+  inputBox.value = modifiedSentence;
 }
 
 //Lower Button
@@ -43,7 +35,6 @@ function lowerBtnClick() {
   inputBox.value = lowerCase;
 }
 
-
 //Upper Button
 
 function upperBtnClick() {
@@ -52,13 +43,11 @@ function upperBtnClick() {
   inputBox.value = upperCase;
 }
 
-
-
-//Capital 
+//Capital
 
 function capitalBtnClick() {
-  sentenceBtnClick()
-  
+  sentenceBtnClick();
+
   let inputValue = inputBox.value;
 
   function capitalizeWords() {
@@ -69,23 +58,16 @@ function capitalBtnClick() {
   }
 
   inputBox.value = capitalizeWords();
-
-
-  
-  
-    
 }
-
-
 
 //Alternate Case
 
-function alternateCase(){
-  sentenceBtnClick()
+function alternateCase() {
+  sentenceBtnClick();
   let inputValue = inputBox.value;
-  function makeEvenLettersUppercase() { 
+  function makeEvenLettersUppercase() {
     var result = "";
-  
+
     for (var i = 0; i < inputValue.length; i++) {
       if (i % 2 === 0) {
         result += inputValue[i].toUpperCase();
@@ -97,10 +79,7 @@ function alternateCase(){
   }
   var modifiedSentence = makeEvenLettersUppercase(inputValue);
   inputBox.value = modifiedSentence;
-  
-  
 }
-
 
 //Title Case
 
@@ -108,7 +87,6 @@ function alternateCase(){
 // let inputValue = inputBox.value;
 
 // }
-
 
 //Inverse
 
@@ -132,16 +110,14 @@ function inverseBtnClick() {
   inputBox.value = inverseCase();
 }
 
-
 //Clear data
 
 function clearData() {
   inputBox.value = "";
-  characterCount.innerHTML= "0000";
+  characterCount.innerHTML = "0000";
   wordCount.innerHTML = "0000";
   lineCount.innerHTML = "0000";
 }
-
 
 //Copy Data
 
@@ -155,23 +131,18 @@ function copyData() {
   document.body.removeChild(temp);
 }
 
-
 //Copy Button Toast
 
-document.addEventListener('copy', function() {
+document.addEventListener("copy", function () {
   // Show the toast notification
-  var toast = document.getElementById('toast');
-  toast.classList.add('show');
+  var toast = document.getElementById("toast");
+  toast.classList.add("show");
 
   // Hide the toast notification after 3 seconds
-  setTimeout(function() {
-    toast.classList.remove('show');
+  setTimeout(function () {
+    toast.classList.remove("show");
   }, 3000);
 });
-
-
-
-
 
 //Counter
 
@@ -179,67 +150,50 @@ document.addEventListener('copy', function() {
 
 let characterCount = document.getElementById("characterCount");
 
-
-inputBox.addEventListener('input', function (e) {
-
+inputBox.addEventListener("input", function (e) {
   // Count the current number of characters
   let currentLength = inputBox.value.length;
 
-  characterCount.innerHTML=currentLength;
-
-
+  characterCount.innerHTML = currentLength;
 });
-
-
 
 //Word Count
 
 let wordCount = document.getElementById("wordCount");
-  inputBox.addEventListener("input", function() {
-    var newText = inputBox.value.replace(/\n/g, "\n ");
-    const words = newText.split(" ");
-    wordCount.innerHTML = words.length;
-    console.log(words);
-    
-  });
-
+inputBox.addEventListener("input", function () {
+  var newText = inputBox.value.replace(/\n/g, "\n ");
+  const words = newText.split(" ");
+  wordCount.innerHTML = words.length;
+  console.log(words);
+});
 
 let inputValue = inputBox.value;
-
-
-  
-
-
 
 //Line Count
 
 let lineCount = document.getElementById("lineCount");
 
-inputBox.addEventListener("keyup", function() {
+inputBox.addEventListener("keyup", function () {
   let lines = inputBox.value.split("\n").length;
   lineCount.innerHTML = lines;
 });
 
-
-
-
-// const loremIpsum = " ";
+const whay = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 // function generateLoremIpsum() {
-//     let numWords = prompt("Enter the number of words for your Lorem Ipsum:");
-//     let words = loremIpsum.split(" ");
-//     let generatedText = "";
-//     for (let i = 0; i < numWords; i++) {
-//         generatedText += words[i] + " ";
-//     }
-//     inputBox.innerHTML = generatedText;
+//   let numWords = prompt("Enter the number of words for your Lorem Ipsum:");
+//   let words = loremIpsum.split(" ");
+//   let generatedText = "";
+//   for (let i = 0; i < numWords; i++) {
+//     generatedText += words[i] + " ";
+//   }
+//   inputBox.innerHTML = generatedText;
 // }
 
-
-// Create a function to generate lorem ipsum
+// // Create a function to generate lorem ipsum
 function generateLoremIpsum(numWords) {
   // Fetch data from the Lorem Ipsum API
-  fetch(`http://loripsum.net/api/${numWords}`)
+  fetch(`https://loripsum.net/api/${numWords}`)
     .then(response => response.text())
     .then(data => {
       // Display the generated Lorem Ipsum
@@ -247,7 +201,6 @@ function generateLoremIpsum(numWords) {
     })
     .catch(error => console.error(error));
 }
-
 
 let numWords = prompt("Enter the number of words you want in your Lorem Ipsum:");
 if(isNaN(numWords)){
